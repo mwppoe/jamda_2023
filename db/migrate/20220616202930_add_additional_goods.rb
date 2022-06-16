524ceebd1cc798f35556adb2c67c891f5fa4694a
Manuel Woletz <manuel.woletz@ppoe.at>
2022-06-16 20:30:44 +0000

    Zusätzliche T-Shirts und Halstücher

---
+++ db/migrate/20220616202930_add_additional_goods.rb
@@ -0,0 +1,14 @@
+# frozen_text_literal: true
+
+#  Copyright (c) 2012-2021, German Contingent for the Worldscoutjamboree 2023. This file is part of
+#  hitobito_jamda_2023 and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/mwppoe/jamda_2023.
+
+class AddAdditionalGoods < ActiveRecord::Migration[4.2]
+    def change
+      add_column :people, :add_good_1, :smallint
+      add_column :people, :add_good_2, :smallint
+      add_column :people, :add_good_3, :smallint
+    end
+end
